510e632a5ebd10a81fc9daf6d0566f1bc30a094e
bootcamp <Zx746794>
2020-04-23 14:42:02 +0300

    matcha: client first commit

---
+++ client/src/test/java/com/matcha/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matcha.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
