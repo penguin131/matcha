504b5bbe5bc73237a85e375e0c67fcddba5f7a7c
bootcamp <Zx746794>
2020-04-25 23:08:04 +0300

    matcha: server +checkUserProfile service

--- matcha-server/src/main/java/com/matcha/server/DatabaseService.java
+++ matcha-server/src/main/java/com/matcha/server/DatabaseService.java
@@ -12,12 +12,15 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.sql.*;
+import java.util.Arrays;
 
 @RestController
 public class DatabaseService {
 
-    //todo фабрику подключений и протестить производительность
+    //todo фабрику подключений и протестить производительность, добавить логирование в случае фейлов
     @Resource
     private DataSource dataSource;
     private Connection connection;
@@ -42,18 +45,22 @@ public class DatabaseService {
                     value = "/createUserProfile",
                     consumes = "application/json")
     @ResponseBody
-    public String addUserProfile(InputStream inputData) {
+    public Boolean addUserProfile(InputStream inputData) {
         try {
             JSONObject jsonObject = new JSONObject(ReadHelper.readJSON(inputData));
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "insert into my_db.t_user_profile (sex, sex_preferences, biography) VALUE (?, ?, ?)");
+                    "insert into my_db.t_user_profile (sex, sex_preferences, biography, password, login) VALUE (?, ?, ?, ?, ?)");
             preparedStatement.setString(1, jsonObject.getString("sex"));
             preparedStatement.setString(2, jsonObject.getString("sex_preferences"));
             preparedStatement.setString(3, jsonObject.getString("biography"));
+            preparedStatement.setString(5, jsonObject.getString("login"));
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(jsonObject.getString("password").getBytes(StandardCharsets.UTF_8));
+            preparedStatement.setString(4, Arrays.toString(hash));
             preparedStatement.execute();
-            return "ok";
+            return true;
         } catch (Exception ex) {
-            return "err:" + ex.getMessage();
+            return false;
         }
     }
 
@@ -85,4 +92,33 @@ public class DatabaseService {
             return "err:" + ex.getMessage();
         }
     }
+
+    /**
+     * Проверит, правильный ли пароль у юзера
+     * @param inputData login, password in JSON
+     * @return true/false
+     */
+    @RequestMapping(method = RequestMethod.GET,
+            value = "/checkUserPassword",
+            consumes = "application/json")
+    @ResponseBody
+    public Boolean checkUserPassword(InputStream inputData) {
+        try {
+            JSONObject jsonObject = new JSONObject(ReadHelper.readJSON(inputData));
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "select password from my_db.t_user_profile where login=?");
+            preparedStatement.setString(1, jsonObject.getString("login"));
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                byte[] hash = digest.digest(jsonObject.getString("password").getBytes(StandardCharsets.UTF_8));
+                return rs.getString(1).equals(Arrays.toString(hash));
+            } else {
+                return false;
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
 }
