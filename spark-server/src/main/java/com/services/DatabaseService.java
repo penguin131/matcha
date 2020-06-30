fd95dd3d1285ae9b749f4476aef487ead0dd9274
bootcamp <Zx746794>
2020-06-30 18:45:07 +0300

    spark-server pom

--- spark-server/src/main/java/com/services/DatabaseService.java
+++ spark-server/src/main/java/com/services/DatabaseService.java
@@ -5,6 +5,7 @@ import static spark.Spark.*;
 public class DatabaseService {
 
     public static void main(String[] args) {
+        port(8888);
         get("/hello", (req, res) -> "Hello World");
     }
 }
