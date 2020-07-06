533f639c95e1d4c9a470f74d762b22b3d3e6076f
bootcamp <Zx746794>
2020-07-06 18:43:48 +0300

    +hash string2

--- spark-server/src/main/java/com/security/SecurityHelper.java
+++ spark-server/src/main/java/com/security/SecurityHelper.java
@@ -3,18 +3,18 @@ package com.security;
 import com.service.DatabaseService;
 import org.apache.log4j.Logger;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 public class SecurityHelper {
     private final static Logger logger = Logger.getLogger(DatabaseService.class);
     public static String generateHash() {
-        int leftLimit = 97;
+        int leftLimit = 48;
         int rightLimit = 122;
-        int targetStringLength = 10;
+        int targetStringLength = 40;
         Random random = new Random();
 
         String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
