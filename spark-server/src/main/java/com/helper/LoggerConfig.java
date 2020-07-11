fe785f83ce2e65d2949d68e4a61835dc851f2744
bootcamp <Zx746794>
2020-07-11 15:49:16 +0300

    +disable file logger

--- spark-server/src/main/java/com/helper/LoggerConfig.java
+++ spark-server/src/main/java/com/helper/LoggerConfig.java
@@ -11,7 +11,7 @@ public class LoggerConfig {
         Properties p = new Properties();
         try {
             p.load(new FileInputStream(Config.getLoggerConfig()));
-            p.setProperty("log4j.appender.file.File", "/Users/bootcamp/logs/log4j-spark-application.log");
+//            p.setProperty("log4j.appender.file.File", "/Users/bootcamp/logs/log4j-spark-application.log");
 //            p.setProperty("log4j.appender.file.File", "/root/logs/log4j-spark-application.log");
             PropertyConfigurator.configure(p);
         } catch (IOException e) {
