e76ef95e22f063c8ccfcd4434511908a640a6daf
bootcamp <Zx746794>
2020-09-15 12:28:47 +0300

    hash log

--- spark-server/src/main/java/com/helper/Password.java
+++ spark-server/src/main/java/com/helper/Password.java
@@ -1,6 +1,7 @@
 package com.helper;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.log4j.Logger;
 
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
@@ -11,10 +12,14 @@ public class Password {
     private static final int iterations = 20*1000;
     private static final int saltLen = 32;
     private static final int desiredKeyLen = 256;
+    private static Logger logger = Logger.getLogger(Password.class);
 
     public static String getSaltedHash(String password) throws Exception {
+        logger.info(String.format("==>  getSaltedHash(%s)", password));
         byte[] salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLen);
-        return Base64.encodeBase64String(salt) + "$" + hash(password, salt);
+        String hash = Base64.encodeBase64String(salt) + "$" + hash(password, salt);
+        logger.info("<==    getSaltedHash(): " + hash);
+        return hash;
     }
 
     //password в изначальном виде, stored хешированный
@@ -29,11 +34,14 @@ public class Password {
     }
 
     private static String hash(String password, byte[] salt) throws Exception {
+        logger.info(String.format("==>  hash(%s, salt)", password));
         if (password == null || password.length() == 0)
             throw new IllegalArgumentException("Empty passwords are not supported.");
         SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
         SecretKey key = f.generateSecret(new PBEKeySpec(
                 password.toCharArray(), salt, iterations, desiredKeyLen));
-        return Base64.encodeBase64String(key.getEncoded());
+        String res = Base64.encodeBase64String(key.getEncoded());
+        logger.info("<==    hash(): " + res);
+        return res;
     }
 }
