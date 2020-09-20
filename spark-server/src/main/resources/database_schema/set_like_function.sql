5c63cc0b9dbbc7b33bd5e51a78b726820516b297
bootcamp <Zx746794>
2020-09-20 21:05:44 +0300

    images

--- spark-server/src/main/resources/database_schema/set_like_function.sql
+++ spark-server/src/main/resources/database_schema/set_like_function.sql
@@ -20,31 +20,44 @@ begin
         where t2.login="from" and t3.login="to")=0
     then
 --         Если лайк ставится повторно, то удаляется
-
-    --     If t_users_unity not exists
-        IF simple=0 and reverse=0
+--             Лайк в обе стороны
+        if (simple>0 and reverse>0)
         then
-    --         insert new user unity
+            update spark_db.t_users_unity set confirmed=false
+            where (select user_profile_id from spark_db.t_user_profile where login="from")=postgres.spark_db.t_users_unity.user2_id
+            and (select user_profile_id from spark_db.t_user_profile where login="to")=postgres.spark_db.t_users_unity.user1_id;
+        else if (simple>0 and reverse=0)
+        then
+--             Лайк в одну сторону
+            delete from spark_db.t_users_unity
+            where postgres.spark_db.t_users_unity.user1_id=(select user_profile_id from spark_db.t_user_profile where login="from")
+            and postgres.spark_db.t_users_unity.user2_id=(select user_profile_id from spark_db.t_user_profile where login="to");
+        --     Если еще нет лайков
+        else IF simple=0 and reverse=0
+        then
+            --         insert new user unity
             insert into spark_db.t_users_unity (user1_id, user2_id)
-                VALUES (
-                    (select user_profile_id from spark_db.t_user_profile where login="from"),
-                    (select user_profile_id from spark_db.t_user_profile where login="to"));
-    --         up rating
+            VALUES (
+                       (select user_profile_id from spark_db.t_user_profile where login="from"),
+                       (select user_profile_id from spark_db.t_user_profile where login="to"));
+            --         up rating
             update spark_db.t_user_profile set rating=rating+1 where login="to";
-    --         if exist reverse t_users_unity
+--             Если есть только лайк в обратную сторону
         elsif simple=0 and reverse>0
         then
             WITH cte_unity AS
-                (select t1.t_users_unity_id
-                from spark_db.t_users_unity t1
-                    join spark_db.t_user_profile t2 on (t2.user_profile_id=t1.user1_id)
-                    join spark_db.t_user_profile t3 on (t3.user_profile_id=t1.user2_id)
-                where t2.login="to" and t3.login="from")
-    --         set confirmed
+                     (select t1.t_users_unity_id
+                      from spark_db.t_users_unity t1
+                               join spark_db.t_user_profile t2 on (t2.user_profile_id=t1.user1_id)
+                               join spark_db.t_user_profile t3 on (t3.user_profile_id=t1.user2_id)
+                      where t2.login="to" and t3.login="from")
+                 --         set confirmed
             update spark_db.t_users_unity set confirmed=true
             where t_users_unity_id in (select * from cte_unity);
-    --         up rating
+            --         up rating
             update spark_db.t_user_profile set rating=rating+1 where login="to";
+        end if;
+        end if;
         end if;
     end if;
 end
