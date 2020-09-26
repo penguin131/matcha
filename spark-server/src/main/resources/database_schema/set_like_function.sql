21bc81d1bb6eb555e381c8b9e984d33f2cb64c54
bootcamp <Zx746794>
2020-09-26 21:58:12 +0300

    set like need fix

--- spark-server/src/main/resources/database_schema/set_like_function.sql
+++ spark-server/src/main/resources/database_schema/set_like_function.sql
@@ -1,39 +1,46 @@
+/**
+  Функция возвращает true, если после выполнения два пользователя подружились. Иначе false
+ */
 create function set_like("from" character varying, "to" character varying) returns boolean
     language plpgsql
 as
 $$
 declare
+--     мой лайк
     simple integer=(select count(*) from spark_db.t_users_unity t1
                                              join spark_db.t_user_profile t2 on (t2.user_profile_id=t1.user1_id)
                                              join spark_db.t_user_profile t3 on (t3.user_profile_id=t1.user2_id)
                     where t2.login="from" and t3.login="to");
+--     меня лайкнули
     reverse integer=(select count(*) from spark_db.t_users_unity t1
                                               join spark_db.t_user_profile t2 on (t2.user_profile_id=t1.user1_id)
                                               join spark_db.t_user_profile t3 on (t3.user_profile_id=t1.user2_id)
                      where t2.login="to" and t3.login="from");
 begin
-    --     Если есть жалоба, то нельзя подружиться
+--               Если есть жалоба, то нельзя подружиться
     if (select count(*) from spark_db.t_complaint t1
                                  join spark_db.t_user_profile t2 on (t1.from_user=t2.user_profile_id)
                                  join spark_db.t_user_profile t3 on (t1.to_user=t3.user_profile_id)
-        where t2.login="from" and t3.login="to")=0
+        where t2.login="from" and t3.login="to")=0 and "from"<>"to"
     then
-        --         Если лайк ставится повторно, то удаляется
---             Лайк в обе стороны
+------------------------------------        LIKED      -----------------------------------------------------------------
+--             пролайкано в обе стороны todo дыра, может не отмениться
         if (simple>0 and reverse>0)
         then
             update spark_db.t_users_unity set confirmed=false
             where (select user_profile_id from spark_db.t_user_profile where login="from")=postgres.spark_db.t_users_unity.user2_id
               and (select user_profile_id from spark_db.t_user_profile where login="to")=postgres.spark_db.t_users_unity.user1_id;
+            --         down rating
             update spark_db.t_user_profile set rating=rating-1 where login="to";
         else if (simple>0 and reverse=0)
         then
---             Лайк в одну сторону
+--             если я уже ставил лайк этому челу
             delete from spark_db.t_users_unity
             where postgres.spark_db.t_users_unity.user1_id=(select user_profile_id from spark_db.t_user_profile where login="from")
               and postgres.spark_db.t_users_unity.user2_id=(select user_profile_id from spark_db.t_user_profile where login="to");
+            --         down rating
             update spark_db.t_user_profile set rating=rating-1 where login="to";
-            --     Если еще нет лайков
+---------------------------------------     NO LIKE     -----------------------------------------------------------------
         else IF simple=0 and reverse=0
         then
             --         insert new user unity
@@ -43,7 +50,7 @@ begin
                        (select user_profile_id from spark_db.t_user_profile where login="to"));
             --         up rating
             update spark_db.t_user_profile set rating=rating+1 where login="to";
---             Если есть только лайк в обратную сторону
+--             Если этот чел уже ставил мне лайк
         elsif simple=0 and reverse>0
         then
             WITH cte_unity AS
@@ -52,7 +59,6 @@ begin
                                join spark_db.t_user_profile t2 on (t2.user_profile_id=t1.user1_id)
                                join spark_db.t_user_profile t3 on (t3.user_profile_id=t1.user2_id)
                       where t2.login="to" and t3.login="from")
-                 --         set confirmed
             update spark_db.t_users_unity set confirmed=true
             where t_users_unity_id in (select * from cte_unity);
             --         up rating
