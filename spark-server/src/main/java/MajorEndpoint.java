95ac618c6ef6dd771d5c09cc8b0b4ac2803d1446
bootcamp <Zx746794>
2020-09-27 16:50:47 +0300

    sss

--- spark-server/src/main/java/MajorEndpoint.java
+++ spark-server/src/main/java/MajorEndpoint.java
@@ -218,8 +218,8 @@ public class MajorEndpoint {
 
 	private static void	addHeaders(Response res) {
 		res.header("Access-Control-Allow-Origin","*");
-		res.header("Access-Control-Allow-Methods","GET, PUT, POST, DELETE");
-		res.header("Access-Control-Allow-Headers","Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+		res.header("Access-Control-Allow-Methods","*");
+		res.header("Access-Control-Allow-Headers","*");
 //		res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 //		res.header("Access-Control-Allow-Origin", "*");
 	}
