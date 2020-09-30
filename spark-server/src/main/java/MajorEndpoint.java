6ad8a8504d39e1de90734f6c7168a651a98d5a44
bootcamp <Zx746794>
2020-09-30 14:22:42 +0300

    fucking cors test

--- spark-server/src/main/java/MajorEndpoint.java
+++ spark-server/src/main/java/MajorEndpoint.java
@@ -24,13 +24,10 @@ import static spark.Spark.*;
 
 public class MajorEndpoint {
 
-	private static final HashMap<String, String> corsHeaders = new HashMap<String, String>();
+	private static final HashMap<String, String> corsHeaders = new HashMap<>();
 
 	static {
-		corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 		corsHeaders.put("Access-Control-Allow-Origin", "*");
-		corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
-		corsHeaders.put("Access-Control-Allow-Credentials", "true");
 	}
 
 	public static void apply() {
