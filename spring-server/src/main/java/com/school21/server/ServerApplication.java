8808a3de9af56afdb86b4ba33c1b3a5150db8f24
penguin131 <zininlol@rambler.ru>
2020-10-08 11:28:33 +0300

    init spring boot

---
+++ spring-server/src/main/java/com/school21/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.school21.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
