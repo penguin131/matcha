52013b766868d2c23abe0f572a1ccdb434f90107
penguin131 <zininlol@rambler.ru>
2020-10-09 17:06:23 +0300

    SecurityHelper.java

--- spring-server/src/main/java/com/school21/server/security/SecurityHelper.java
+++ spring-server/src/main/java/com/school21/server/security/SecurityHelper.java
@@ -1,20 +1,20 @@
 package com.school21.server.security;
 
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Component
 public class SecurityHelper {
     private final static Logger logger = Logger.getLogger(SecurityHelper.class);
-    public static String generateHash() {
+    public static String generateHash(int hashSize) {
         int leftLimit = 48;
         int rightLimit = 122;
-        int targetStringLength = 40;
         Random random = new Random();
-
         String generatedString = random.ints(leftLimit, rightLimit + 1)
                 .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
+                .limit(hashSize)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
         logger.info("generateHash(): " + generatedString);
