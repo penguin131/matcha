77057e439503ae1486583893fabe75bd7159eac8
18574058 <bvyachentsov@sberbank.ru>
2020-10-10 11:21:51 +0300

    Fiex filters.

--- client/client/src/components/forms/FilterForm/FilterForm.js
+++ client/client/src/components/forms/FilterForm/FilterForm.js
@@ -10,13 +10,14 @@ const FilterForm = ({ filters, setFilters }) => {
   return (
     <Form
       onSubmit={e => {
-        const tagsArr = e.tags?.replace(/\s+/g, '').trim().split(',') || []
+        const tagsString = e.tags?.replace(/\s+/g, '').trim()
+        const tagsArr = tagsString.length > 0 ? tagsString.split(',') : []
         setFilters({...filters, ...e, tags: tagsArr})
       }}
       initialValues={{
         ageGap: null,
         ratingGap: null,
-        tags: [],
+        tags: '',
         locationGap: null,
       }}
       render={({handleSubmit, submitting, pristine, values }) => (
